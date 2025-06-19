6932b2ddca98836add617d579325802440407c93
Anshan Singh <aarpitsingh246@gmail.com>
2025-06-19 17:57:10 +0530

    Add a some javascript to make for better ui experience

--- app.js
+++ app.js
@@ -9,3 +9,51 @@
     .then(data => {
       document.getElementById("footer-placeholder").innerHTML = data;
     });
+
+    // vetables.html
+
+  document.addEventListener("DOMContentLoaded", function () {
+    // Target all boxes with the class
+    document.querySelectorAll(".custom-box").forEach(function (box) {
+      box.style.cursor = "pointer"; // change cursor to pointer
+
+      box.addEventListener("click", function () {
+        const url = box.getAttribute("data-url"); // e.g., "vegetables.html"
+        if (url) {
+          window.location.href = url; // redirect
+        }
+      });
+    });
+  });
+
+
+  // oil html
+
+    document.addEventListener("DOMContentLoaded", function () {
+    // Target all boxes with the class
+    document.querySelectorAll(".flex-week").forEach(function (box) {
+      box.style.cursor = "pointer"; // change cursor to pointer
+
+      box.addEventListener("click", function () {
+        const url = box.getAttribute("data-url"); // e.g., "vegetables.html"
+        if (url) {
+          window.location.href = url; // redirect
+        }
+      });
+    });
+  });
+
+  // sticky navbar
+
+   window.addEventListener("scroll", function () {
+    const stickyElement = document.getElementById("stickyElement");
+    const stickyOffset = stickyElement.offsetTop;
+
+    if (window.scrollY > stickyOffset) {
+      stickyElement.classList.add("fixed");
+    } else {
+      stickyElement.classList.remove("fixed");
+    }
+  });
+
+
